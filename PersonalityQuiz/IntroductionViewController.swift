bf7ea4df041e3d00a41ce7dd3382d5207bec2d4d
AcaroMan <fabienfabio5@gmail.com>
2022-10-19 00:02:07 +0100

    Initial Commit

---
+++ PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by mac on 12/10/2022.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
