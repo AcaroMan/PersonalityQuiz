713caeb96bbadb9f580dcc1b4bb9db23f185922b
AcaroMan <82720436+AcaroMan@users.noreply.github.com>
2022-10-19 17:28:23 +0100

    Changes

--- PersonalityQuiz/QuestionViewController.swift
+++ PersonalityQuiz/QuestionViewController.swift
@@ -76,6 +76,31 @@ class QuestionViewController: UIViewController {
     }
     
     @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+        let currentAnswers = questions[questionIndex].answers
+
+        switch sender{
+        case singleButton1:
+            answersChosen.append(currentAnswers[0])
+        case singleButton2:
+            answersChosen.append(currentAnswers[1])
+        case singleButton3:
+            answersChosen.append(currentAnswers[2])
+        case singleButton4:
+            answersChosen.append(currentAnswers[3])
+        default:
+            break
+        }
+        nextQuestion()
+    
+    }
+    func nextQuestion(){
+        questionsIndex += 1
+        if(questionIndex<questions.count){
+            updateUI()
+        }
+        else{
+            // performSegue(withIdentifier: "ResultsSegue", sender: nil)
+        }
     }
     
     func updateUI(){
