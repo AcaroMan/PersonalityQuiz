6f06d5d1fe249ffffbadad496f8834608fdabe59
AcaroMan <82720436+AcaroMan@users.noreply.github.com>
2022-10-19 22:44:38 +0100

    Changes

--- PersonalityQuiz/ResultsViewController.swift
+++ PersonalityQuiz/ResultsViewController.swift
@@ -13,6 +13,7 @@ class ResultsViewController: UIViewController {
     var responses : [Answer]
     
     init?(coder: NSCoder, responses:[Answer]){
+
         self.responses = responses
         super.init(coder: coder)
     }
@@ -20,6 +21,8 @@ class ResultsViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
