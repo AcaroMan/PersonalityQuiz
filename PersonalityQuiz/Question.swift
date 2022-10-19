055d151aa0b85c1a2d0c48c8138ea40980241b66
AcaroMan <2220647@my.ipleiria.pt>
2022-10-19 01:20:23 +0100

    Questions Updated

---
+++ PersonalityQuiz/Question.swift
@@ -0,0 +1,42 @@
+//
+//  Question.swift
+//  PersonalityQuiz
+//
+//  Created by mac on 19/10/2022.
+//
+
+import Foundation
+
+
+struct Question{
+    var text: String
+    var type: ResponseType
+    var answers:[Answer]
+}
+
+enum ResponseType {
+    case single,multiple,ranged
+}
+
+struct Answer{
+    var text: String
+    var type: AlcoholicType
+}
+
+enum AlcoholicType{
+    case hydroHomie, casualDrinker,alcoholLover,anonAlcoholic
+    var definition:String{
+        switch self{
+        case .hydroHomie:
+            return "You dont drink much. Only drink your 1.5 liter+o of daily water and despise anyone who drinks anything else."
+        case .casualDrinker:
+            return "You like to casually drink those soft drinks like beer. You might drink a few of them or just go with 15 pints in a row."
+        case .alcoholLover:
+            return "Damn this wine is really good- you say to yourself while you eat your meal because you cant a single meal without drinking a few glasses."
+        case .anonAlcoholic:
+            return "What a sad fucker you must be. Drinking alone those full glasses of vodka while feeling depressed about life."
+            <#code#>
+        }
+    }
+}
+
--- PersonalityQuiz/QuestionViewController.swift
+++ PersonalityQuiz/QuestionViewController.swift
@@ -15,7 +15,40 @@ class QuestionViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    var questions : [Question] = [
+        Question (
+            text: "A very interesting question",
+            type: .ranged,
+            answers: [
+                Answer(text: "1", type: .hydroHomie),
+                Answer(text: "2", type: .casualDrinker),
+                Answer(text: "3", type: .alcoholLover),
+                Answer(text: "4", type: .anonAlcoholic),
+            ]
+        ),
+        Question (
+            text: "What is your favourite beverage?",
+            type: .single,
+            answers: [
+                Answer(text: "Water", type: .hydroHomie),
+                Answer(text: "Beer", type: .casualDrinker),
+                Answer(text: "Wine", type: .alcoholLover),
+                Answer(text: "Vodka", type: .anonAlcoholic),
+            ]
+        ),
+        Question (
+            text: "What places do you go to, to drink?",
+            type: .multiple,
+            answers: [
+                Answer(text: "Water Fountain", type: .hydroHomie),
+                Answer(text: "Coffee/Pub, type: .casualDrinker),
+                Answer(text: "Tavern", type: .alcoholLover),
+                Answer(text: "House", type: .anonAlcoholic),
+            ]
+        )
 
+
+    ]
     /*
     // MARK: - Navigation
 
